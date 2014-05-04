728eefb5db136fac49a45c9991bdaa2c5a95be04
Adam Merry <adammerry@adams-mbp.gateway.2wire.net>
2014-05-04 01:16:24 -0700

    [CHORE] Fixed typo in article model.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5} 
-  validates :body, presence: true, length: {minimum: 2}
+  validates :text, presence: true, length: {minimum: 2}
 end
