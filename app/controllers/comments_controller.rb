775c7ab34d9bd268a4365c41cb8acdb09116e37a
Adam Merry <adammerry@Adams-MacBook-Pro.local>
2014-05-03 18:34:25 -0700

    [FEATURE] Added comment validation. Cleaned up ui.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,11 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    if @comment.errors.any?
+      render "articles/show"
+    else
+      redirect_to article_path(@article)
+    end
   end
   
   def destroy
